24df4007c8a579bb7c07d4dfe77cd1e9a58d81d2
NidulaJay <nidulajpjayasinghe@gmail.com>
2023-12-20 18:21:00 +0530

    link class in servlet

--- src/main/java/productaddservlet.java
+++ src/main/java/productaddservlet.java
@@ -7,6 +7,8 @@ import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
 import java.io.IOException;
 import java.io.PrintWriter;
+
+import code.dbconnection;
 /**
  * Servlet implementation class productaddservlet
  */
