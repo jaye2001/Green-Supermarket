89c2e09324f9541635c944ef1fc5a4d8f520c91c
Sithil <sithil2005113@gmail.com>
2023-12-25 12:30:02 +0530

    home servlets and classes

--- src/main/java/Product.java
+++ src/main/java/Product.java
@@ -1,20 +1,38 @@
 package com.green;
 
 public class Product {
-
+	
+	private int id;
 	private String image;
 	private String name;
+	private String description;
+	private float price;
 	
-	public void Set_values(String _image, String _name) {
+	public void Set_values(int _id, String _image, String _name, String _description, float _price) {
 		
+		id = _id;
 		image = _image;
 		name = _name;
+		description = _description;
+		price = _price;
+		
 	}
+	public int GetId() {
+		return id;
+	}
+	
 	public String GetImage() {
 		return image;
 	}
 	public String GetName() {
 		return name;
 	}
+	public String GetDescription() {
+		return description;
+	}
+	public float GetPrice() {
+		return price;
+	}
+
 	
 }
